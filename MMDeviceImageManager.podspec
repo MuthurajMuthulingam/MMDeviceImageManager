f2ae0dff56aec8c1dc2770d48aa64d0d44bff22b
Muthuraj Muthulingam <muthurajmuthulingam@gmail.com>
2026-02-22 08:27:41 +0530

    migrate swift concurrency and iOS 18 minimum deployment target

--- MMDeviceImageManager.podspec
+++ MMDeviceImageManager.podspec
@@ -26,7 +26,7 @@ Pod::Spec.new do |s|
   s.source           = { :git => 'https://github.com/MuthurajMuthulingam/MMDeviceImageManager.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
-  s.ios.deployment_target = '8.0'
+  s.ios.deployment_target = '18.0'
   s.swift_version = '4.0'
 
   s.source_files = 'MMDeviceImageManager/Classes/**/*'
--- MMDeviceImageManager/Classes/MMDeviceImageManager.swift
+++ MMDeviceImageManager/Classes/MMDeviceImageManager.swift
@@ -11,7 +11,7 @@ import Photos
 import AVFoundation
 
 // MARK: - ImagePickerManager Rules
-public protocol MMDeviceImageManagerDelegate: class {
+public protocol MMDeviceImageManagerDelegate: AnyObject {
     func mmDeviceImageManager(_ imagePicker: MMDeviceImageManager, selectedImage imageInfo:[String:Any])
     func mmDeviceImageManagerError(_ imagePicker: MMDeviceImageManager, error: ImagePickerErrorType)
 }
@@ -34,7 +34,7 @@ public enum ImagePickerErrorType {
 }
 
 public class MMDeviceImageManager: NSObject {
-    
+
     lazy private var imagePickerController: UIImagePickerController = UIImagePickerController()
     private var imagePickerMode: ImagePickerMode = .photoGallery {
         didSet {
@@ -58,21 +58,21 @@ public class MMDeviceImageManager: NSObject {
     }
     
     public func hasPhotoGalleryPermission() -> Bool {
-        return photoGalleryAccess(from: PHPhotoLibrary.authorizationStatus())
+        return photoGalleryAccess(from: PHPhotoLibrary.authorizationStatus(for: .readWrite))
     }
     
     public func hasCameraAccess() -> Bool {
-        return cameraAccess(from: AVCaptureDevice.authorizationStatus(for: .video))
+        cameraAccess(from: AVCaptureDevice.authorizationStatus(for: .video))
     }
     
     public func isDeviceHasCamera() -> Bool {
-        return UIImagePickerController.isSourceTypeAvailable(.camera)
+        UIImagePickerController.isSourceTypeAvailable(.camera)
     }
     
     // MARK: - Private Helpers
     private func initializeImagePicker() {
         imagePickerController.delegate = self
-        let mediaType: UIImagePickerControllerSourceType = (imagePickerMode == .photoGallery) ? .photoLibrary : .camera
+        let mediaType: UIImagePickerController.SourceType = (imagePickerMode == .photoGallery) ? .photoLibrary : .camera
         imagePickerController.sourceType = mediaType
     }
     
@@ -97,98 +97,97 @@ public class MMDeviceImageManager: NSObject {
             actionSheet.addAction(cameraAction)
             actionSheet.addAction(cancelAction)
             viewController?.present(actionSheet, animated: true, completion:nil)
-            break
         default:
             break
         }
     }
     
     private func handleCameraOption() {
-        if self.isDeviceHasCamera() {
-            if self.hasCameraAccess() { // already access given
-                // access requested and granted
+        Task { [weak self] in
+            guard let self = self else { return }
+            if !self.isDeviceHasCamera() {
+                self.delegate?.mmDeviceImageManagerError(self, error: .noCameraAvailable)
+                return
+            }
+            let hasAccess = self.hasCameraAccess()
+            let granted: Bool
+            if hasAccess {
+                granted = true
+            } else {
+                granted = await self.requestCameraAccessAsync()
+            }
+            if granted {
                 self.imagePickerMode = .camera
                 self.presentImagePicker()
             } else {
-                self.requestCameraAccess(completion: { status in
-                    if status {
-                        // access requested and granted
-                        self.imagePickerMode = .camera
-                        self.presentImagePicker()
-                    } else { // no access
-                        self.delegate?.mmDeviceImageManagerError(self, error: .noCameraPermission)
-                    }
-                })
+                self.delegate?.mmDeviceImageManagerError(self, error: .noCameraPermission)
             }
-        } else {
-            self.delegate?.mmDeviceImageManagerError(self, error: .noCameraAvailable)
         }
     }
     
     private func handlePhotoGalleryOption() {
         self.imagePickerMode = .photoGallery
-        // handle photoGallery option
-        if self.hasPhotoGalleryPermission() {
-            // access available
-            self.presentImagePicker()
-        } else {
-            // request access
-            self.requestPhotoLibrarayPermission(completion: { (status) in
-                if status {
-                    // access given, Go ahead and present the Gallery
-                    self.presentImagePicker()
-                } else {
-                    self.delegate?.mmDeviceImageManagerError(self, error: .noPhotoGalleryPermission)
-                }
-            })
+        Task { [weak self] in
+            guard let self else { return }
+            let hasAccess = self.hasPhotoGalleryPermission()
+            let granted: Bool
+            if hasAccess {
+                granted = true
+            } else {
+                granted = await self.requestPhotoLibraryPermission()
+            }
+            if granted {
+                self.presentImagePicker()
+            } else {
+                self.delegate?.mmDeviceImageManagerError(self, error: .noPhotoGalleryPermission)
+            }
         }
     }
     
     private func cameraAccess(from authorizationStatus: AVAuthorizationStatus) -> Bool {
-        var status: Bool = false
-        switch authorizationStatus {
-        case .authorized:
-            status = true
-        default:
-            break
-        }
-        return status
+        authorizationStatus == .authorized
     }
     
     private func photoGalleryAccess(from status: PHAuthorizationStatus) -> Bool {
-        var status: Bool = false
-        switch PHPhotoLibrary.authorizationStatus() {
-        case .authorized:
-            status = true
-        default:
-            break
-        }
-        return status
+        status == .authorized || status == .limited
     }
     
-    private func requestPhotoLibrarayPermission(completion : @escaping ((_ status:Bool) -> ())) {
-        PHPhotoLibrary.requestAuthorization {[unowned self] (status) in
-            let isAccessGiven = self.photoGalleryAccess(from: status)
-            completion(isAccessGiven)
+    private func requestPhotoLibraryPermission() async -> Bool {
+        let status = await withCheckedContinuation { (continuation: CheckedContinuation<PHAuthorizationStatus, Never>) in
+            PHPhotoLibrary.requestAuthorization(for: .readWrite) { status in
+                continuation.resume(returning: status)
+            }
         }
+        return photoGalleryAccess(from: status)
     }
     
-    private func requestCameraAccess(completion: @escaping ((_ status: Bool) -> Void)) {
-        AVCaptureDevice.requestAccess(for: .video) { status in
-            completion(status)
+    private func requestCameraAccessAsync() async -> Bool {
+        await withCheckedContinuation { continuation in
+            AVCaptureDevice.requestAccess(for: .video) { status in
+                continuation.resume(returning: status)
+            }
         }
     }
 }
 
 // MARK: - Image Picker Delegates
 extension MMDeviceImageManager: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        picker.dismiss(animated: true, completion: nil)
-        self.delegate?.mmDeviceImageManager(self, selectedImage: info)
+    public func imagePickerController(
+        _ picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any])
+    {
+        picker.dismiss(animated: true) {
+            let stringKeyedInfo: [String: Any] = info.reduce(into: [:]) { partialResult, pair in
+                partialResult[pair.key as String] = pair.value
+            }
+            self.delegate?.mmDeviceImageManager(self, selectedImage: stringKeyedInfo)
+        }
     }
     
     public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        picker.dismiss(animated: true, completion: nil)
-        self.delegate?.mmDeviceImageManagerError(self, error: .cancel)
+        picker.dismiss(animated: true) {
+            self.delegate?.mmDeviceImageManagerError(self, error: .cancel)
+        }
     }
 }
+
